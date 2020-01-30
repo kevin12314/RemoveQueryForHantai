6cef9b63e76ad23298d9a5bcd912a4f039c6a656
kevin12314 <he00251287@hotmail.com>
2020-01-30 10:55:33 +0800

    Update 使連結不帶query字串.user.js
    調整include，使用正規表達式

--- 使連結不帶query字串.user.js
+++ 使連結不帶query字串.user.js
@@ -9,19 +9,38 @@
 // @include       http://www.toonsex.pics/*
 // @include       http://www.freexxxtoon.com/*
 // @include       http://www.xxx3dcomix.com/*
-// @include       http://www.3dhentaicomics.com/*
-// @include       http://www.sexhentai.pro/*
-// @include       http://www.xxxhentaivids.com/*
 // @include       http://www.animexxxmovies.com/*
 // @include       http://www.porncomix.pro/*
 // @include       http://www.cartoonsexcomix.com/*
-// @include       http://www.xxxhentaipics.pro/*
+// @include       /^http:\/\/www\.{1}\w*hentai\w*\.\w*\/.*$/
 // @grant        none
 // ==/UserScript==
 
+var flowWebsite=['www.porncomics.me',
+                 'www.3dhentaicomics.com'];
+
 (function() {
     'use strict';
+
+    window.setInterval(( () => {
+        if(window.onbeforeunload!=null){
+            window.onbeforeunload=null;
+        }
+    } ), 1000);
+
+     // debugger;
+
     var imagelist=document.querySelectorAll("div.portfolio-post>a");
+
+    if(isFlowWeb(imagelist)){
+        window.setInterval(( () => {
+            imagelist=document.querySelectorAll("div.portfolio-post>a");
+            if(imagelist.length>0){
+                removeQuery(imagelist);
+            }
+        } ), 1000);
+    }
+
     if(imagelist.length==0){
         imagelist=document.querySelectorAll("div.thumbs-block a");
     }
@@ -30,12 +49,16 @@
     if(imagelist.length==0){
         imagelist=document.querySelectorAll("div#portfolio-items>ul a[data-id^='code']");
 
-        window.setInterval(( () => {
-            imagelist=document.querySelectorAll("div#portfolio-items>ul a[data-id^='code']");
-            if(imagelist.length>0){
-                removeQuery(imagelist);
-            }
-        } ), 1000);
+        if(isFlowWeb(imagelist)){
+            window.setInterval(( () => {
+                imagelist=document.querySelectorAll("div#portfolio-items>ul a[data-id^='code']");
+                //var test=document.querySelectorAll("div#portfolio-items>ul li");
+                //console.debug(test);
+                if(imagelist.length>0){
+                    removeQuery(imagelist);
+                }
+            } ), 1000);
+        }
     }
 
     //http://www.sexhentai.pro/*
@@ -43,13 +66,12 @@
         imagelist=document.querySelectorAll("div.image-list a");
     }
 
-    removeQuery(imagelist);
+    if(imagelist.length==0){
+        imagelist=document.querySelectorAll("div.gallery-list a");
+    }
 
-    window.setInterval(( () => {
-            if(window.onbeforeunload!=null){
-                window.onbeforeunload=null;
-            }
-        } ), 1000);
+
+    removeQuery(imagelist);   
 
     /*
     setTimeout(function(){
@@ -74,4 +96,12 @@ function removeQuery(imagelist){
             item.href=item.href.substring(0,queryIndex);
         }
     });
+}
+
+function isFlowWeb(imagelist){
+    if(imagelist.length==0){
+        return false;
+    }
+    var check=flowWebsite.filter(p=>location.href.indexOf(p)>=0);
+    return check.length>0
 }
